7b2403882ac1d866a60c078cae8be3b40572f54f
John Foushee <johnfoushee@gmail.com>
2013-09-06 16:45:44 -0400

    Removing the getter/setter methods hydration method for entity forms.

--- module/Application/src/Application/Stdlib/Hydrator/DoctrineObject.php
+++ module/Application/src/Application/Stdlib/Hydrator/DoctrineObject.php
@@ -17,40 +17,4 @@ use Zend\Code\Reflection\ClassReflection;
 class DoctrineObject extends \DoctrineModule\Stdlib\Hydrator\DoctrineObject
 {
 
-	/**
-	 * Extract values from an object using a by-value logic (this means that it uses the entity
-	 * API, in this case, getters)
-	 *
-	 * @param  object $object
-	 * @throws RuntimeException
-	 * @return array
-	 */
-	protected function extractByValue($object)
-	{
-		/**
-		 * Get all properties (even unannotated)
-		 */
-		$properties = $this->metadata->getReflectionClass()->getProperties();
-
-		$methods = get_class_methods($object);
-
-		$data = array();
-		foreach ($properties as $property) {
-			$fieldName = $property->getName();
-			$getter = 'get' . ucfirst($fieldName);
-
-			// Ignore unknown fields
-			if (!in_array($getter, $methods)) {
-				continue;
-			}
-			$fieldValue = $object->$getter();
-			if ($fieldValue instanceof \Application\Entity\Base && $fieldValue->isNull()) {
-				$fieldValue = null;
-			}
-			$data[$fieldName] = $this->extractValue($fieldName, $fieldValue);
-		}
-
-		return $data;
-	}
-
 }
